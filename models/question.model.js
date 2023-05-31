f139b24a71e5c4c107674dccb54eabb55cb708af
fredmelka <fred.melka@gmail.com>
2023-05-31 23:38:49 +0200

    question model added

---
+++ models/question.model.js
@@ -0,0 +1,50 @@
+
+import { Schema, model } from 'mongoose';
+import User from './user.model.js'; 
+
+const NON_EMPTY_STRING_REGEX = /^(?!\s*$).+/;
+const PICTURE_URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
+const capitalize = (string) => string[0].toUpperCase() + string.substring(1);
+
+// Creating Schema for the Subdocument 'answer' 
+const answerSchema = new Schema({
+    _text: {type: String, required: true, trim: true},
+    _value: {type: Number, required: true, default: 0}
+    },
+    {timestamps: true}
+);
+
+const questionSchema = new Schema({
+    _tags: [        // Here it is an array of strings !!
+            {type: String,
+            match: NON_EMPTY_STRING_REGEX,
+            uppercase: true}
+            ],
+    _level: {
+            type: String,
+            required: true,
+            enum: ['Easy', 'Intermediate', 'Hard']},
+    _label: {
+            type: String,
+            required: true,
+            trim: true,
+            set: capitalize},
+    _answers: [     // Here it is an array of subdocuments !!
+            {type: answerSchema, 
+            default: {}, // OR Try this syntax to match Mongoose's documentation about passing default as turnaround: 'default: () => ({})'
+            select: false}
+            ],
+    _picture: {
+            type: String,
+            match: PICTURE_URL_REGEX},
+    _owner: {
+            type: Schema.Types.ObjectId,
+            ref: User,
+            required: true}
+    },
+    {timestamps: true}
+);
+
+const Question = model('Question', questionSchema);
+
+export default Question;
--- models/user.model.js
+++ models/user.model.js
@@ -1,12 +1,15 @@
 
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
 const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+// FRONTEND CONTROL : Passwords must be 8 characters long, contain one of each uppercase, lowercase, digit and special character.
+// PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 const userSchema = new Schema({
     _username: {
             type: String,
-            required: [true, 'username with 8 characters minimum is required'],
+            required: [true, 'Username must be eight to twenty characters long.'],
             unique: true,
             lowercase: true,
             trim: true,
@@ -14,11 +17,11 @@ const userSchema = new Schema({
             maxLength: 20},
     _password: {
             type: String,
-            required: [true, 'password with 10 characters minimum is required'],
+            required: [true, 'Password must have ten characters minimum.'],
             select: false},
     _email: {
             type: String,
-            required: [true, 'a valid email is required'],
+            required: [true, 'Valid email is required.'],
             unique: true,
             lowercase: true,
             trim: true,
