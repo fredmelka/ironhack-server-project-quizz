d156cc67e01630c2085800bdb5ec41bd4d4dd49d
fredmelka <fred.melka@gmail.com>
2023-06-06 03:08:24 +0200

    slight update of signup route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -23,31 +23,31 @@ let {_username, _password, _email} = request.body;
 
 // Rejection for empty inputs
 if (_username == '' || _password == '' || _email == '') {
-    response.status(400).json({success: false, message: 'Bad Request: Please provide non empty inputs.'});
+    response.status(400).json({success: false, message: 'Please provide non empty inputs.'});
     return;};
 
 // Rejection for invalid email format
 let EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 if (!EMAIL_REGEX.test(_email)) {
-    response.status(400).json({success: false, message: 'Bad Request: Please provide a valid email.'});
+    response.status(400).json({success: false, message: 'Please provide a valid email.'});
     return;};
 
 // Rejection for invalid password format (Rules: 8 chars min at least one A-a-9-@)
 let PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 if (!PASSWORD_REGEX.test(_password)) {
-    response.status(400).json({success: false, message: 'Bad Request: Password must be 8 characters minimum of which at least one uppercase, lowercase, digit and special.'});
+    response.status(400).json({success: false, message: 'Password must have 8 characters minimum of which at least one uppercase, lowercase, digit and special.'});
     return;};
 
 // Rejection for invalid _username length
 if (_username.length < 8 || _username.length > 20) {
-    response.status(400).json({success: false, message: 'Bad Request: Username must be 8 to 20 characters long.'});
+    response.status(400).json({success: false, message: 'Username must be 8 to 20 characters long.'});
     return;};
 
 try {
     let foundUser = await User.findOne({_email: _email});
     
     // Rejection for _email already in use
-    if (foundUser) {response.status(400).json({success: false, message: 'Bad Request: User with such email already exists.'}); return;};
+    if (foundUser) {response.status(400).json({success: false, message: 'User with such email already exists.'}); return;};
 
     // Email is as of now supposedly new and server can proceed to sign up
     let generatedSalt = await bcrypt.genSalt(saltRounds);
@@ -61,7 +61,7 @@ try {
     let newUser = {user, email, role, id};
     
     // Send success response
-    response.status(201).json({success: true, user: newUser});
+    response.status(201).json({success: true, message: newUser});
 }
 catch (error) {console.log(error); next(error);};
 };
